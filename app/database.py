17672e44ddef9ab09dee1235ed4b9308a58ae940
PayamAnari <anari.p62@gmail.com>
2024-03-27 14:10:07 +0100

    Create models.py

--- app/database.py
+++ app/database.py
@@ -5,3 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:2219499@localhost/fast-api"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
