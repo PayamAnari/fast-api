fad4c975bf2da772e37e493635e19e91412e9639
PayamAnari <anari.p62@gmail.com>
2024-03-27 14:22:15 +0100

    Modify main.py

--- app/main.py
+++ app/main.py
@@ -2,12 +2,23 @@ from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
-from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from . import models
+from .database import engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 class Post(BaseModel):
