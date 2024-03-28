53510045809a28b2a2cf03a562e93c3485eed2b7
PayamAnari <anari.p62@gmail.com>
2024-03-28 16:29:45 +0100

    Modify get user by id

--- app/main.py
+++ app/main.py
@@ -111,6 +111,12 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 
-@app.get("/users{id}", response_model=list[schemas.UserOut])
-def get_user(id: int):
-    
+@app.get("/users/{id}", response_model=list[schemas.UserOut])
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"user with id: {id} was not found",
+        )
+    return [user]
