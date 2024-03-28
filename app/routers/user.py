9f3848e60b0070bcf4ecdcea0f52e818bc5a8663
PayamAnari <anari.p62@gmail.com>
2024-03-28 20:14:48 +0100

    Modify user route

---
+++ app/routers/user.py
@@ -0,0 +1,74 @@
+from .. import models, schemas, utils
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Response, status, HTTPException, Depends
+from ..database import engine, get_db
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+
+    new_user = models.User(
+        **user.dict(),
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get("/users", response_model=list[schemas.UserOut])
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+
+    return users
+
+
+@app.get("/users/{id}", response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"user with id: {id} was not found",
+        )
+    return user
+
+
+@app.put("/users/{id}", response_model=schemas.UserOut)
+def update_user(
+    id: int, updated_user: schemas.UserCreate, db: Session = Depends(get_db)
+):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"user with id: {id} was not found",
+        )
+    hashed_password = utils.hash(updated_user.password)
+    updated_user.password = hashed_password
+
+    for key, value in updated_user.dict().items():
+        setattr(user, key, value)
+
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+
+@app.delete("/users/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id)
+
+    if user.first() is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"user with id: {id} was not found",
+        )
+
+    user.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
