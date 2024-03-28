ee2ad1509cce0322d91bffb9edee7a2e5a94518c
PayamAnari <anari.p62@gmail.com>
2024-03-28 16:32:30 +0100

    Create get all users

--- app/main.py
+++ app/main.py
@@ -111,7 +111,14 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 
-@app.get("/users/{id}", response_model=list[schemas.UserOut])
+@app.get("/users", response_model=list[schemas.UserOut])
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+
+    return users
+
+
+@app.get("/users/{id}", response_model=schemas.UserOut)
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
@@ -119,4 +126,4 @@ def get_user(id: int, db: Session = Depends(get_db)):
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"user with id: {id} was not found",
         )
-    return [user]
+    return user
