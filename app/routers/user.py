718b883b41671a204c18ea7586f74988ffab4681
PayamAnari <anari.p62@gmail.com>
2024-03-29 20:48:10 +0100

    Add error handeling to create user

--- app/routers/user.py
+++ app/routers/user.py
@@ -12,6 +12,15 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
 
+    existing_user = (
+        db.query(models.User).filter(models.User.email == user.email).first()
+    )
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with this email already exists",
+        )
+
     new_user = models.User(
         **user.dict(),
     )
