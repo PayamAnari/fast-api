85cd965ef7048b5f4126212ba9f1676eb1d2475b
PayamAnari <anari.p62@gmail.com>
2024-03-29 11:21:58 +0100

    Add auth router

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from ..database import get_db
 
 
-router = APIRouter(prefix="/auth", tags=["Authentications"])
+router = APIRouter(tags=["Authentications"])
 
 
 @router.post("/login")
