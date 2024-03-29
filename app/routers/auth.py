335e449a61a5cf389eb506fd1de98611dd73a953
PayamAnari <anari.p62@gmail.com>
2024-03-29 11:44:35 +0100

    Correction auth

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, Response
-from .. import schemas, models, utils
+from .. import schemas, models, utils, oauth2
 from sqlalchemy.orm import Session
 from ..database import get_db
 
@@ -21,4 +21,9 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(status_code=404, detail=f"Invalid credentials")
 
-    return {"message": "Login successfully"}
+    access_token = oauth2.create_access_token(data={"sub": user.email})
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+    }
