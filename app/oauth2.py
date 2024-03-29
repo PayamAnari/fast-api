543d432bfc2c36d2d4b7cf0c2c0a3728f3facac6
PayamAnari <anari.p62@gmail.com>
2024-03-29 12:04:22 +0100

    create verify access token

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,7 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from . import schemas
+
 
 SECRET_KEY = "09d25e094faa65432456r466b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
@@ -12,3 +14,16 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+
+        token_data = schemas.TokenData(email=email)
+    except JWTError:
+        raise credentials_exception
+    return token_data
