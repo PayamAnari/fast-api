35235325875c54d5b5eb82c2de4401beb5e8ec54
PayamAnari <anari.p62@gmail.com>
2024-03-31 21:15:03 +0200

    Create likes table

--- app/models.py
+++ app/models.py
@@ -46,3 +46,13 @@ class User(Base):
         onupdate=text("now()"),
         nullable=False,
     )
+
+
+class Likes(Base):
+    __tablename__ = "likes"
+    user_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
+    )
+    post_id = Column(
+        Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True
+    )
