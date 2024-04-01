a975297c9a16237fb307ea31e5cc404e73e730ed
PayamAnari <anari.p62@gmail.com>
2024-04-01 14:16:40 +0200

    Add favorite route

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .routers import post, user, auth, like
+from .routers import post, user, auth, like, favorite
 from .config import settings
 
 models.Base.metadata.create_all(bind=engine)
@@ -13,3 +13,4 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(like.router)
+app.include_router(favorite.router)
